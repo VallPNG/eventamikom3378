74d25546aaf97b949fb08e0ebc6548f165a5b409
VALENZ <valedhikafikri@students.amikom.ac.id>
2026-05-08 18:12:43 +0700

    Tugas 4

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -6,10 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    protected $fillable = ['name', 'slug'];
-
-    public function events()
-    {
-        return $this->hasMany(Event::class);
-    }
+    //
+    public function events() {
+    return $this->hasMany(Event::class);
+}
 }
